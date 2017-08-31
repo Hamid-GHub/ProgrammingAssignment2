a4e01a84d986c21c34c9149c5878a771730bf7a2
Hamid-GHub <31422096+Hamid-GHub@users.noreply.github.com>
2017-08-31 11:44:39 +0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this fuction gets a matrix and returns a list of functions
+## the matrix object has functions below: 
+##set: inputs a matrix 
+##get: returns a matrix
+##get_inverse: outputs the cached inverted matrix
+##set_inverse: caches an already calculated inverted matrix (by function: cachesolve)
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) 
+{
 
-makeCacheMatrix <- function(x = matrix()) {
+          inv=NULL
+          set<-function(y)
+          {
+                      x<<-y
+                      inv<<-NULL
+                 
+          }
+          get<-function () x
+          set_inverse<-function(i) inv<<-i
+          get_inverse<-function() inv
+          list(set=set,get=get,set_inverse=set_inverse,get_inverse=get_inverse)
+    
 
-}
+  
+ }
 
 
-## Write a short comment describing this function
+## This function checks if the inversion already calculated. If yes, retrieves 
+## the cached inverted matrix. If no it calculates the inverted matrix using solve(x)
+##and returns i : the calculated inverted matrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+        i<-x$get_inverse()
+        if(!is.null(i))
+        {
+                    message("getting cached data")
+                    return(i)
+          
+        }
+        matrix<-x$get()
+        i<-solve(matrix)
+        x$set_inverse(i) 
+        i
+  
 }
